99a08c1dc6206a67aa8f4a0113d6447671358714
prasimbaal <prasimbaal@gmail.com>
2024-05-15 13:12:28 +0100

    head edits

--- inc_head.php
+++ inc_head.php
@@ -3,8 +3,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0 -->
 <link rel="stylesheet" media="screen" href="assets/styles/style.css" />
-<link rel="shortcut icon" href="media/favicon.ico" type="image/x-icon">
-<link rel="address bar icon" href="media/favicon.ico" type="image/x-icon">
 
 <script src="assets\js\bootstrap.min.js"></script>
 <script src="assets/js/jquery-1.4.4.min.js" type="text/javascript"></script>
